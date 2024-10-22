d4795b6dcdfe5d42ec9f437da598a84cf3c53b72
Khaalid <faarhjaamac@gmail.com>
2024-10-22 13:54:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log(`Hello`)
+// console.log(`I Like Pizza!`)
+
+// window.alert(`This is an alert!`)
+// window.alert(`I Like Pizza`)
+
+document.getElementById(`myH1`).textContent = `Hello World`
+document.getElementById(`myPr`).textContent = `I Wanna Be a Web Developer`
+
+//This is a Comment
