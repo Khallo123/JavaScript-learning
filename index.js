0eb47aec2537a784263e4ff2bcce9cd51df148d1
Khaalid <faarhjaamac@gmail.com>
2024-10-23 06:14:25 +0300

    second

--- index.js
+++ index.js
@@ -4,7 +4,28 @@
 // window.alert(`This is an alert!`)
 // window.alert(`I Like Pizza`)
 
-document.getElementById(`myH1`).textContent = `Hello World`
-document.getElementById(`myPr`).textContent = `I Wanna Be a Web Developer`
+// document.getElementById(`myH1`).textContent = `Hello World`
+// document.getElementById(`myPr`).textContent = `I Wanna Be a Web Developerg`
 
 //This is a Comment
+
+// let x = 123
+// console.log(`123`);
+
+let age = 34
+let price = 19.09
+let gba = 2.0
+
+let favoritFood = "Checken"
+let firstName = "Khallo"
+
+console.log(typeof firstName)
+console.log(typeof favoritFood)
+console.log(typeof price)
+console.log(typeof age)
+console.log(typeof gba)
+console.log(`I like ${favoritFood}`)
+console.log(`My Name is ${firstName}`)
+console.log(`you are ${age} years old`)
+console.log(`The price is ${price}`)
+console.log(`This is your rate ${gba}`)
