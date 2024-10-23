9cdc9885d370fe992c55fe2343df78a4c3afe604
Khaalid <faarhjaamac@gmail.com>
2024-10-23 06:53:04 +0300

    done

--- index.js
+++ index.js
@@ -12,20 +12,37 @@
 // let x = 123
 // console.log(`123`);
 
-let age = 34
-let price = 19.09
-let gba = 2.0
 
-let favoritFood = "Checken"
-let firstName = "Khallo"
+let online = true
+let forSale = true
+let isStudent = false
 
-console.log(typeof firstName)
-console.log(typeof favoritFood)
-console.log(typeof price)
-console.log(typeof age)
-console.log(typeof gba)
-console.log(`I like ${favoritFood}`)
-console.log(`My Name is ${firstName}`)
-console.log(`you are ${age} years old`)
-console.log(`The price is ${price}`)
-console.log(`This is your rate ${gba}`)
+console.log(`Khallo is online: ${online}`)
+console.log(`Is This car for sale: ${forSale}`)
+console.log(`Khaalid:${isStudent}`)
+
+// let favoritFood = "Checken"
+// let firstName = "Khallo"
+// let email = "Khallo@gmail.com"
+
+// console.log(typeof firstName)
+// console.log(typeof favoritFood)
+// console.log(typeof email)
+
+
+// console.log(`I like ${favoritFood}`)
+// console.log(`My Name is ${firstName}`)
+// console.log(`your email is ${email}`)
+
+// let age = 34
+// let price = 19.09
+// let gba = 2.0
+
+// console.log(typeof price)
+// console.log(typeof age)
+// console.log(typeof gba)
+
+// console.log(`you are ${age} years old`)
+// console.log(`The price is ${price}`)
+// console.log(`This is your rate ${gba}`)
+
